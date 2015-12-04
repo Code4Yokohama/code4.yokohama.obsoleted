c5dfce9d03b04b879d827ae0e489ef1b0057f4cf
Souta Ito <soutaito@info-lounge.jp>
2015-12-04 19:16:51 +0900

    Add hackathon page.

---
+++ hackathon/js/script.js
@@ -0,0 +1,32 @@
+$(function(){
+
+    var winW = window.innerWidth,
+        winH = window.innerHeight;
+
+    //bxslider
+    $('.bxslider, .sp_bxslider').bxSlider({
+        auto: true,
+        mode: 'fade',
+        controls: true,
+        pager: true,
+        speed:  1000,
+        pause:  5000,
+        easing: 'ease-in-out'
+    });
+
+    fixKvSize(winW,winH);
+
+    $(window).resize(function(){
+        var winW = window.innerWidth;
+        fixKvSize(winW,winH);
+    });
+
+});
+
+function fixKvSize(winW,winH){
+
+    $('.bx-viewport, .bx-wrapper, .bxslider, .bxslider li').css({
+        'height': winH
+    });
+
+}

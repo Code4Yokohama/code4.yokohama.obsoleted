d331bc91ee13eaad092b4abf0fa65299c0c06e12
Naoko Usui <usui@info-lounge.jp>
2016-01-08 19:03:32 +0900

    add yokosuka_content, add tournament_content, fix report layout position

--- hackathon/js/script.js
+++ hackathon/js/script.js
@@ -10,4 +10,21 @@ $(function(){
         easing: 'ease-in-out'
     });
 
+    //ページ内リンク
+    function inpageLink() {
+        $('a[href^=#]').click(function (e) {
+            e.preventDefault();
+            var speed = 500;
+            var href = $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top;
+
+            $("html, body").animate({scrollTop: position}, speed, "swing");
+            return false;
+        });
+    }
+
+    //読み込み完了後に実行
+    inpageLink();
+
 });

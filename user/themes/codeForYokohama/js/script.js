c019c7f88321bde4e3712d7fb5858e115b1f1a12
Miyuu Okabe <meetan625@gmail.com>
2017-12-26 16:54:38 +0900

    delete comment

--- user/themes/codeForYokohama/js/script.js
+++ user/themes/codeForYokohama/js/script.js
@@ -4,12 +4,4 @@ jQuery(function($) {
     $('.menu_item_box').slideToggle();
     $(this).toggleClass("active");
   })
-
-  // $('.menu_icon').click(function() {
-  //   $('.menu_item_box').show();
-  //   return false;
-  // });
-  // if (click  {
-  //
-  // }
 });

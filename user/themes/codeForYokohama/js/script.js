11765a7e0b6d8e22bdb5aa8aae501e9a93ba9b38
Miyuu Okabe <meetan625@gmail.com>
2017-12-26 16:49:47 +0900

    add js

---
+++ user/themes/codeForYokohama/js/script.js
@@ -0,0 +1,15 @@
+jQuery(function($) {
+
+  $('.menu_icon').on('click',function(){
+    $('.menu_item_box').slideToggle();
+    $(this).toggleClass("active");
+  })
+
+  // $('.menu_icon').click(function() {
+  //   $('.menu_item_box').show();
+  //   return false;
+  // });
+  // if (click  {
+  //
+  // }
+});

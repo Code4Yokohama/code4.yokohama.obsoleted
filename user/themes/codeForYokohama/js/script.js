86003cda110f1a84211ba0dab9fab3e11e78e3dd
Miyuu Okabe <meetan625@gmail.com>
2018-01-05 15:55:59 +0900

    update members detail

--- user/themes/codeForYokohama/js/script.js
+++ user/themes/codeForYokohama/js/script.js
@@ -8,18 +8,17 @@ jQuery(function($) {
   $('.members_info').on('click', function() {
     var targetId = $(this).attr('id');
     var targetBox = $(this).parents('.members_list').data('target-box-id');
-    console.log(targetBox);
-    // TODO: クリックしたときにどこのdetail_boxに表示させるのか書く！！！
-    // TODO: 隠しBox(members_detail_content)をdetail_boxにコピーする
-    // TODO: 閉じるボタンでDetailを閉じる
 
-    // if ($('#members_detail-' + targetId).hasClass('isOpen')) {
-    //   $('#members_detail-' + targetId).slideUp(300).removeClass('isOpen');
-    // } else {
-    //   $('.isOpen').slideUp(300).removeClass('isOpen');
-    //   window.setTimeout(function() {
-    //     $('#members_detail-' + targetId).slideDown(300).addClass('isOpen');
-    //   }, 500);
-    // }
+    if ($('*[data-box-id="' + targetBox + '"]').hasClass('isOpen')) {
+      $('*[data-box-id="' + targetBox + '"]').slideUp(300).removeClass('isOpen');
+    } else {
+      $('.isOpen').slideUp(300).removeClass('isOpen');
+      window.setTimeout(function() {
+        console.log($('#members_detail-' + targetId));
+        $('*[data-box-id="' + targetBox + '"]')
+          .html($('#members_detail-' + targetId))
+          .slideDown(300).addClass('isOpen');
+      }, 500);
+    }
   })
 });

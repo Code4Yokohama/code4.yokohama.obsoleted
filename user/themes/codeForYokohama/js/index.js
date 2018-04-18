d3810e0fc924a8cf798cd8152217670750b172df
Souta Ito <soutaito@info-lounge.jp>
2018-04-18 18:06:00 +0900

    Update templates

--- user/themes/codeForYokohama/js/index.js
+++ user/themes/codeForYokohama/js/index.js
@@ -1,26 +1,25 @@
-$(function($) {
+$(function ($) {
 
   //トップページでSPのときのみ行う調整
-  var winW = $(window).width();
+  var winW = $(window).width()
   if (750 >= winW) {
 
     //SPのMVの高さ調整
-    var winH = $(window).height();
-    var headerH = $('.header_box_sp').height();
-    var mvH = $('.main_visual_typo_sp').height();
-    var mvTop = (winH-headerH)/2 + mvH/2;
-    $('#mainVisual').css('height', winH+'px');
-    $('.main_visual_typo_sp').css('top', mvTop+'px');
+    var winH = $(window).height()
+    var headerH = $('.header_box_sp').height()
+    var mvH = $('.main_visual_typo_sp').height()
+    var mvTop = (winH - headerH) / 2 + mvH / 2
+    $('#mainVisual').css('height', winH + 'px')
+    $('.main_visual_typo_sp').css('top', mvTop + 'px')
 
-    //#memberのスライダー
-    $('#lightSlider').lightSlider({
-      item:2,
-      loop:true,
-      slideMove:2,
-      easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-      speed:600
-    });
+    // //#memberのスライダー
+    // $('#lightSlider').lightSlider({
+    //   item:2,
+    //   loop:true,
+    //   slideMove:2,
+    //   easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+    //   speed:600
+    // });
 
   }
-
-});
+})
--- user/themes/codeForYokohama/js/script.js
+++ user/themes/codeForYokohama/js/script.js
@@ -6,35 +6,35 @@ $(function($) {
     $(this).toggleClass("active");
   })
 
-  $('.members_info').on('click', function(e) {
-
-    var targetId = $(this).attr('id');
-    var targetBox = $(this).parents('.members_list').data('target-box-id');
-    var currentOpen = $('.members_detail_box.isOpen > .members_detail_content').attr('id');
-    var windowWidth = $(window).width();
-
-    if (windowWidth >= 750) {
-      e.preventDefault();
-      if (currentOpen === undefined) {
-        $('*[data-box-id="' + targetBox + '"]')
-          .html($('#members_detail-' + targetId).clone())
-          .slideDown(300).addClass('isOpen');
-        $('#box_triangle-' + targetId).slideDown(300).addClass('isOpen');
-      } else {
-        $('.members_detail_box.isOpen').slideUp(300).removeClass('isOpen');
-        $('.box_triangle.isOpen').slideUp(300).removeClass('isOpen');
-        if (targetId !== currentOpen.replace(/^members_detail-/g, '')) {
-          $('.members_detail_box.isOpen').slideUp(300).removeClass('isOpen');
-          $('.box_triangle.isOpen').slideUp(300).removeClass('isOpen');
-          window.setTimeout(function() {
-            $('*[data-box-id="' + targetBox + '"]')
-              .html($('#members_detail-' + targetId).clone())
-              .slideDown(300).addClass('isOpen');
-            $('#box_triangle-' + targetId).slideDown(300).addClass('isOpen');
-          }, 500);
-
-        }
-      }
-    }
-  })
+  // $('.members_info').on('click', function(e) {
+  //
+  //   var targetId = $(this).attr('id');
+  //   var targetBox = $(this).parents('.members_list').data('target-box-id');
+  //   var currentOpen = $('.members_detail_box.isOpen > .members_detail_content').attr('id');
+  //   var windowWidth = $(window).width();
+  //
+  //   if (windowWidth >= 750) {
+  //     e.preventDefault();
+  //     if (currentOpen === undefined) {
+  //       $('*[data-box-id="' + targetBox + '"]')
+  //         .html($('#members_detail-' + targetId).clone())
+  //         .slideDown(300).addClass('isOpen');
+  //       $('#box_triangle-' + targetId).slideDown(300).addClass('isOpen');
+  //     } else {
+  //       $('.members_detail_box.isOpen').slideUp(300).removeClass('isOpen');
+  //       $('.box_triangle.isOpen').slideUp(300).removeClass('isOpen');
+  //       if (targetId !== currentOpen.replace(/^members_detail-/g, '')) {
+  //         $('.members_detail_box.isOpen').slideUp(300).removeClass('isOpen');
+  //         $('.box_triangle.isOpen').slideUp(300).removeClass('isOpen');
+  //         window.setTimeout(function() {
+  //           $('*[data-box-id="' + targetBox + '"]')
+  //             .html($('#members_detail-' + targetId).clone())
+  //             .slideDown(300).addClass('isOpen');
+  //           $('#box_triangle-' + targetId).slideDown(300).addClass('isOpen');
+  //         }, 500);
+  //
+  //       }
+  //     }
+  //   }
+  // })
 });
